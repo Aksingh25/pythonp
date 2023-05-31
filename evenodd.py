8451e01ac7494e2e1455896751a2316a0d0e481f
Aksingh25 <102855178+Aksingh25@users.noreply.github.com>
2023-05-31 21:06:32 +0530

    Add files via upload

---
+++ evenodd.py
@@ -0,0 +1,19 @@
+def count_odd_even(numbers):
+    odd_count = 0
+    even_count = 0
+    
+    for num in numbers:
+        if num % 2 == 0:
+            even_count += 1
+        else:
+            odd_count += 1
+    
+    return odd_count, even_count
+
+
+# Example usage
+number_list = [2, 3, 4, 55, 56, 78, 75, 69, 66, 101, 100]
+odd, even = count_odd_even(number_list)
+
+print("Number of odd numbers:", odd)
+print("Number of even numbers:", even)
